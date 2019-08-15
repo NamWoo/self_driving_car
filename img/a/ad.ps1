50e6782482eb37ea5eb7b22c3087b1850b1c58c5
namwoo <pre3ice@gmail.com>
2019-08-16 02:13:28 +0900

    add

---
+++ img/a/ad.ps1
@@ -0,0 +1,20 @@
+﻿Write-Host "Congratulations! Your first script executed successfully!"
+Get-Date
+
+
+Write-Host "Self_Driving_Car start!"
+cd D:\!Google_Learning\083_Self_Driving_Car\!GItup
+git pull origin master
+git add .
+git commit -m "AT-Add"
+git push -u origin master
+Write-Host "Self_Driving_Car done!"
+
+
+Write-Host "Self_Driving_Car start!"
+cd D:\!Google_Learning\083_Self_Driving_Car\!self_driving_car_my
+git pull origin master
+git add .
+git commit -m "AT-Add"
+git push -u origin master
+Write-Host "Self_Driving_Car_my done!"
